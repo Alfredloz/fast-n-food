3d7ccafbeb78b1a635f98069141dd65a3ea288e8
Alessandro <internicola.alessandro@gmail.com>
2021-03-20 13:23:49 +0100

    Modificato PaymentController, aggiunta la rotta per checkout
    all'interno del page controller,

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -11,55 +11,50 @@ use Illuminate\Support\Facades\App;
 class PaymentController extends Controller
 {
     public function make(Request $request)
-    {   
+    {
+      // la request contiene i dati per autorizzare il pagamento, ma non quelli
+      // del pagamento stesso come il prezzo o altro
+      $payload = $request->input('payload', false);
+      $nonce = $payload['nonce'];
+      $amount = '76.46';
+
+      $status = Braintree\Transaction::sale([
+        'amount' => $amount,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+          'submitForSettlement' => True
+        ]
+      ]);
+
+      return response()->json($status);
+      // response rimanda indietro diversi dati tra cui l'amount e la data della transazione
+    }
+}
+
+
+        // parte OLD, la lascio per sicurezza al momento
+
         // richiamo ENV parametri
 
-        $env= Env::get('BRAINTREE_ENV');
-        $merchantId = Env::get('BRAINTREE_MERCHANT_ID');
-        $publicKey = Env::get('BRAINTREE_PUBLIC_KEY');
-        $privateKey = Env::get('BRAINTREE_PRIVATE_KEY');
+        // $env= Env::get('BRAINTREE_ENV');
+        // $merchantId = Env::get('BRAINTREE_MERCHANT_ID');
+        // $publicKey = Env::get('BRAINTREE_PUBLIC_KEY');
+        // $privateKey = Env::get('BRAINTREE_PRIVATE_KEY');
 
-        
-        
         // Braintree Gateway
-        
-        $gateway = new Braintree\Gateway([
-            'environment' => $env,
-            'merchantId' => $merchantId,
-            'publicKey' => $publicKey,
-            'privateKey' => $privateKey
-            ]);
 
-
-
-        $nonce = $gateway->paymentMethodNonce();    
-        
-        dd($nonce);
-
-
-            
-        // $token = 'sandbox_mfrr887s_7cxbszb7hywbswm5';
+        // $gateway = new Braintree\Gateway([
+        //     'environment' => $env,
+        //     'merchantId' => $merchantId,
+        //     'publicKey' => $publicKey,
+        //     'privateKey' => $privateKey
+        //     ]);
+        //
+        // $nonce = $gateway->paymentMethodNonce();
+        // dd($nonce);
 
         // $result = $gateway->paymentMethodNonce()->create('A_PAYMENT_METHOD_TOKEN');
         // $nonce = $result->paymentMethodNonce->nonce;
-
-
-        // $status = Braintree\Transaction::sale([
-        // 'amount' => '10.00',
-        // 'paymentMethodNonce' => 'fake-valid-nonce',
-        // 'options' => [
-        //     'submitForSettlement' => True
-        // ]
-        // ]);
-         
         // return response()->json($status);
-
-        // dd($status);
-        // return response()->json($status);
-        // return response()->json(Braintree\ClientToken::generate());
-    
-
         // $clientToken = $gateway->clientToken()->generate();
-        // dd($clientToken);   
-    }
-}
+        // dd($clientToken);
