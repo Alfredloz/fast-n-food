d4355dcb630d1dc77020b2a2c1ec0e63ea4d959c
gabriele <gabrielescodinu96@gmail.com>
2021-03-21 11:30:28 +0100

    style create plate form

--- resources/views/admin/plates/create.blade.php
+++ resources/views/admin/plates/create.blade.php
@@ -2,64 +2,72 @@
 
 
 @section('content')
-@include('layouts.partials.errors')
+<div class="login-container">
+    <h1>Create your Dish</h1   >
+    @include('layouts.partials.errors')
 
-<h2>Create your plate</h2>
+    <form action="{{route('admin.plates.store')}}" method="post" enctype="multipart/form-data">
+        @csrf
 
-<form action="{{route('admin.plates.store')}}" method="post" enctype="multipart/form-data">
-    @csrf
+        {{-- Input text name --}}
+        <div class="input-container">
+            <!-- <label for="name">Name</label> -->
+            <i class="fas fa-pizza-slice"></i><input placeholder="Name" class="form-control" type="text" name="name" id="name" value="{{ old('name')}}">
+        </div>
+        @error('name')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
 
-    {{-- Input text name --}}
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input class="form-control" type="text" name="name" id="name" value="{{ old('name')}}">
-    </div>
-    @error('name')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+        @error('slug')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <hr>
 
-    @error('slug')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+        {{-- Input number price --}}
+        <div class="input-container">
+            <!-- <label for="price">Price</label> -->
+            <i class="fas fa-tags"></i><input placeholder="Price" class="form-control" type="number" name="price" id="price" step="0.01" min="0" max="9999.99" value="{{ old('price')}}">
+        </div>
+        @error('price')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <hr>
 
+        {{-- Input textarea description --}}
+        <div class="input-container">
+            <!-- <label for="description_ingredients">Description</label> -->
+            <textarea placeholder="Description" class="form-control" type="text" rows="3" name="description_ingredients" id="description_ingredients">{{old('description_ingredients')}}</textarea>
+        </div>
+        @error('description_ingredients')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <hr>
 
-    {{-- Input textarea description --}}
-    <div class="form-group">
-        <label for="description_ingredients">Description</label>
-        <textarea class="form-control" type="text" rows="3" name="description_ingredients" id="description_ingredients">{{old('description_ingredients')}}</textarea>
-    </div>
-    @error('description_ingredients')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+        {{-- Input file picture --}}
+        <div class="input-container">
+            <i class="fas fa-sign-in-alt"></i><label for="picture" class="col-md-4 col-form-label text-md-right">Add image</label>
+            <input type="file" class="form-control-file" name="picture" id="picture" value="{{ old('picture')}}">
+        </div>
+        <hr>
 
-    {{-- Input number price --}}
-    <div class="form-group">
-        <label for="price">Price</label>
-        <input class="form-control" type="number" name="price" id="price" step="0.01" min="0" max="9999.99" value="{{ old('price')}}">
-    </div>
-    @error('price')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+        {{-- Input radio visibility --}}
+        <div class="form-check my-4">
+            <input type="radio" class="form-check-input" name="visibility" value="1" checked>
+            <label for="visibility" class="form-check-label">Available</label>
+            <br>
+            <input type="radio" class="form-check-input" name="visibility" value="0">
+            <label for="visibility" class="form-check-label">Not Available</label>
+        </div>
+        @error('visibility')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+        <hr>
 
-    {{-- Input radio visibility --}}
-    <div class="form-check my-4">
-        <input type="radio" class="form-check-input" name="visibility" value="1" checked>
-        <label for="visibility" class="form-check-label">Available</label>
-        <br>
-        <input type="radio" class="form-check-input" name="visibility" value="0">
-        <label for="visibility" class="form-check-label">Not Available</label>
-    </div>
-    @error('visibility')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+        {{-- Submit button --}}
+        <div class="login-container">
+            <button type="submit" class="login-button" name="submit">Submit</button>
+        </div>
+    </form>
 
-    {{-- Input file picture --}}
-    <div class="form-group">
-        <label for="picture">Add image</label>
-        <input type="file" class="form-control-file" name="picture" id="picture" value="{{ old('picture')}}">
-    </div>
-
-    {{-- Submit button --}}
-    <button type="submit" class="btn btn-success" name="submit">Submit</button>
-</form>
+</div>
 @endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Fast n Food</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -85,5 +85,7 @@
             @yield('content')
         </main>
     </div>
+
+    @include('layouts.footer')
 </body>
 </html>
