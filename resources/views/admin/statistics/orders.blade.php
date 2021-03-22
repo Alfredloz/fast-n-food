b98c5d3b8f3ba2b04d5c72fbcee375041ca427ce
Mirko <mirko.rinelli@gmail.com>
2021-03-22 13:29:26 +0100

    Chartjs integrato nel progetto

--- resources/views/admin/statistics/orders.blade.php
+++ resources/views/admin/statistics/orders.blade.php
@@ -3,6 +3,35 @@
 
 @section('content')
 
-  <h1>Statistica degli ordini singoli singoli in cui si vedono i dettagli dei singoli ordini</h1>
+<h1>Statistica degli ordini singoli singoli in cui si vedono i dettagli dei singoli ordini</h1>
+<div class="card">
+  <div class="card-body">
+    <canvas id="myChart"></canvas>
+  </div>
+</div>
+
+<script type="application/javascript">
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'red',
+            borderColor: 'blue',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+</script>
 
 @endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -19,9 +19,10 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
-
 </head>
 <body>
     <div id="app">
