94d6fcdd350a8e17cc3189e91564aaeb7863c8ed
Mirko <mirko.rinelli@gmail.com>
2021-03-25 17:34:42 +0100

    Stile home dashboard e show

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,14 +4,15 @@
 
   <div class="presentation">
     <h1>Benvenuto nella tua personale dashboard</h1>
-    <p>Da qui puoi gestire il tuo ristorante: puoi creare, modificare o cancellare tutti i tuoi piatti e specialità direttamente dal tuo Menù</p>
-    <p>Inoltre puoi controllare le tue statistiche di vendita e degli ultimi 12 mesi e l'elenco completo di tutti i piatti venduti.</p>
+    <hr>
+    <p>Ciao Boss, da qui puoi gestire il tuo ristorante: puoi creare, modificare o cancellare tutti i tuoi piatti e specialità direttamente dal tuo Menù.</p>
+    <p>Inoltre puoi controllare le tue statistiche di vendita degli ultimi 12 mesi e l'elenco completo di tutti i piatti venduti.</p>
   </div>
 
   <div class="dashboard_menu">
-    <a class="img_dashboard" href="{{ route('admin.plates.index')}}" role="button"><img src="{{asset('/images/back_office/menu.png')}}" alt=""></a>
-    <a class="img_dashboard" href="{{ route('admin.orders')}}" role="button"><img src="{{asset('/images/back_office/analytics.png')}}" alt=""></a>
-    <a class="img_dashboard" href="{{ route('admin.sold')}}" role="button"><img src="{{asset('/images/back_office/invoice.png')}}" alt=""></a>
+    <a class="img_dashboard" href="{{ route('admin.plates.index')}}" role="button"><img src="{{asset('/images/back_office/menu.png')}}" alt=""><h3>Menu</h3></a>
+    <a class="img_dashboard" href="{{ route('admin.orders')}}" role="button"><img src="{{asset('/images/back_office/analytics.png')}}" alt=""><h3>Statistiche</h3></a>
+    <a class="img_dashboard" href="{{ route('admin.sold')}}" role="button"><img src="{{asset('/images/back_office/invoice.png')}}" alt=""><h3>Ordini ricevuti</h3></a>
   </div>
 
   
