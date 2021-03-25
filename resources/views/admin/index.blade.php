7a0f803a7871edb7d1edc0d93b7f2e66fc2df5e9
Mirko <mirko.rinelli@gmail.com>
2021-03-25 16:22:38 +0100

    Modifica navbar nel bottone di log in e stile della dashboard home

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.dashboard')
 
 @section('content')
-  <h1>DASHBOARD</h1>
-  <h2>My resturant</h2>
 
-  <a class="btn btn-primary my-3" href="{{ route('admin.plates.index')}}" role="button">Menu</a>
-
-  <div class="mt-5">
-    <h2>My statistics</h2>
-    <p>General trend of the restaurant</p>
-  <a class="btn btn-primary my-3" href="{{ route('admin.orders')}}" role="button">Orders</a>
-  <a class="btn btn-primary my-3" href="{{ route('admin.sold')}}" role="button">Sold</a>
+  <div class="presentation">
+    <h1>Benvenuto nella tua personale dashboard</h1>
+    <p>Da qui puoi gestire il tuo ristorante: puoi creare, modificare o cancellare tutti i tuoi piatti e specialità direttamente dal tuo Menù</p>
+    <p>Inoltre puoi controllare le tue statistiche di vendita e degli ultimi 12 mesi e l'elenco completo di tutti i piatti venduti.</p>
   </div>
+
+  <div class="dashboard_menu">
+    <a class="img_dashboard" href="{{ route('admin.plates.index')}}" role="button"><img src="{{asset('/images/back_office/menu.png')}}" alt=""></a>
+    <a class="img_dashboard" href="{{ route('admin.orders')}}" role="button"><img src="{{asset('/images/back_office/analytics.png')}}" alt=""></a>
+    <a class="img_dashboard" href="{{ route('admin.sold')}}" role="button"><img src="{{asset('/images/back_office/invoice.png')}}" alt=""></a>
+  </div>
+
+  
 
 @endsection
